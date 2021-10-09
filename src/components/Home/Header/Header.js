367717fe27d9e9f4903a22db743a44be67bbbd4c
Rony163 <minhajulislamrony43@gmail.com>
2021-10-09 12:00:29 +0600

    get user info

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../../hooks/useFirebase';
 import './Header.css';
 
 const Header = () => {
+    const { user } = useFirebase();
     return (
         <div className="header">
             <Link to="/home">Home</Link>
             <Link to="/register">Register</Link>
             <Link to="/login">Login</Link>
-            <button>Sign Out</button>
+            {user?.displayName && <button>Log Out</button>}
         </div>
     );
 };
